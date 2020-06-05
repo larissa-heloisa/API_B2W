c59865168b99d9ae028d3f8f4ef91d9faa074e45
Larissa Heloisa <larissaheloisalh@gmail.com>
2020-06-04 23:16:27 -0300

    Project Created

---
+++ src/main/java/com/larissaheloisa/apib2w/Apib2wApplication.java
@@ -0,0 +1,13 @@
+package com.larissaheloisa.apib2w;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apib2wApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apib2wApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/larissaheloisa/apib2w/Apib2wApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larissaheloisa.apib2w;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apib2wApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
